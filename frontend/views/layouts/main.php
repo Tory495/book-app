f4574fb1fb8d127a0eb557d902fb0a62d3b41b6d
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2026-03-05 00:56:56 +0300

    Update navbar

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -38,6 +38,8 @@ AppAsset::register($this);
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Books', 'url' => ['/book/index']],
+        ['label' => 'Authors', 'url' => ['/author/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
